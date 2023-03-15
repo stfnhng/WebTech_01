53c825c4b30c06a8daa48c8a8dfa67a63de6ccb2
suraj <ninja-66@live.nl>
2023-03-15 01:40:47 +0100

    get all elements

--- menus.js
+++ menus.js
@@ -8,31 +8,39 @@ var sections = document.querySelectorAll("section");
 var articles = document.querySelectorAll("article");
 //get the footer element
 var footer = document.querySelectorAll("footer");
+
+//check what elements appear in these variables above
 console.log(header);
 console.log(body);
 console.log(sections);
 console.log(articles);
 console.log(footer);
 
+console.log("");
+// make an array of arrays to make create options for all the tags in just one go
 var element_collection = [header,body,sections,articles,footer];
-//console.log(element_collection);
-console.log("")
-for (i = 0; i<element_collection.length;i++){
-    
-    for(var bruh of element_collection[i])
-         console.log(bruh)
-    
-}
 
-// for (var bruh of element_collection){
-//     for(var bruh2 of bruh ){console.log(bruh2);}
-//     console.log(bruh.id);
-//     var option = document.createElement("option");
-//     var optiontext = document.createTextNode(bruh.id);
-//     //console.log(option.innerHTML);
-//     option.appendChild(optiontext);
-//     select.appendChild(option);
-// }
+//get the select elemnt for the first menu
+var first_menu = document.getElementById("first_menu");
+//check if we got the right element
+console.log(first_menu);
+
+//create a option tag for all the elements that we want to be able to changed.
+console.log("");
+
+for (var part_of_page of element_collection){
+    
+    for(var element_of_page of part_of_page){
+        
+        var option = document.createElement("option");
+        var option_text = document.createTextNode(element_of_page.id);
+        option.appendChild(option_text);
+        first_menu.appendChild(option);
+        console.log(element_of_page);
+    }
+}
+//alleen deze wordt aangeroepen tot nu to 
+//TODO: weghalen
 function select_element(){
     var index = select.selectedIndex;
     //window.scrollTo(0, document.getElementById(filler))
@@ -42,7 +50,7 @@ function select_element(){
     
     console.log(document.getElementById(select[index].innerHTML));
 }
-
+//TODO: weghalen
 function change_styling() {
     document.querySelectorAll("section")[index].childNodes[1].style.fontSize="3em";
     var test = document.querySelectorAll("section")[index];
