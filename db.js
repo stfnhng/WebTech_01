9b2a3001315ec6a6fb0f49cec70f3a28d61908cb
suraj <ninja-66@live.nl>
2023-04-03 16:42:57 +0200

    automatically create databse, create table and add data

--- db.js
+++ db.js
@@ -1,25 +1,61 @@
-// This file will be used to create the .db-file and all the code are commented out after the operation
+var fs = require("fs");
+var file =__dirname + "/" + "database.db";
+//check if the file exist in this folder
+var exists = fs.existsSync(file);
+
+//check if the file exists
+if(!exists){
+    fs.openSync(file, "w")
+    console.log("does not exist")
+}
 
 const sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
 let sql;
 
+db.serialize(function(){
+    if(!exists){
+        db.run("CREATE TABLE movies(id INTEGER PRIMARY KEY AUTOINCREMENT, title, genre, year, director, rating)")
+    }
+})
+
+var stmt = db.prepare( "INSERT INTO movies VALUES (NULL,?,?,?,?,?)");
+stmt.run("Shrek", "Animation/Comedy",2001, "Andrew Adamson", "7,9");
+stmt.run("Shrek", "Comedy",2002, "Andrew Adamsssssson", "3,0");
+stmt.finalize();
+db.each("SELECT * FROM movies", function(err,row){
+console.log(row);
+})
+db.close();
+
+// This file will be used to create the .db-file and all the code are commented out after the operation
+
+
 //connect to DB
-const db = new sqlite3.Database("./database.db",sqlite3.OPEN_READWRITE,(err) => {
-    if (err) return console.error(err.message);
-});
+// const db = new sqlite3.Database("./database.db",sqlite3.OPEN_READWRITE,(err) => {
+//     if (err) return console.error(err.message);
+// });
 
 //create movie table
-// sql = `CREATE TABLE movies(movie_id INTEGER PRIMARY KEY AUTOINCREMENT, title, genre, year, director, rating)`
-//   db.run(sql);
+// sql = `CREATE TABLE movies(movie_id INTEGER PRIMARY KEY AUTOINCREMENT, title, genre, year, director, rating)`;
+// db.run(sql);
 
 //drop table
 //db.run("DROP TABLE movies")
 
 
 //insert data into table
-sql = `INSERT INTO movies(movie_id, title, genre, year, director, rating) VALUES (?,?,?,?,?,?)`
-db.run(sql);
+// sql = `INSERT INTO movies(movie_id, title, genre, year, director, rating) VALUES (?,?,?,?,?,?)`
+// db.run(sql);
 
-db.run(sql, [999, "Shrek", "Animation/Comedy",2001, "Andrew Adamson", "7,9" ],(err) => {
-    if (err) return console.error(err.message);
-});
+// db.run(sql, [ "Shrek", "Animation/Comedy",2001, "Andrew Adamson", "7,9" ],(err) => {
+//     if (err) return console.error(err.message);
+// }
+
+// sql = `SELECT * FROM movies`;
+// db.all(sql,[],(err,rows)=>{
+//     if(err)return console.error(err.message);
+//     rows.forEach(row=>{
+//         console.log(row)
+//     })
+// })
