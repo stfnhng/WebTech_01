d379c92eb3ee6db9c361a1b721af1f03e22045b6
Stefan Huang <123669812+stfnhng@users.noreply.github.com>
2023-04-13 13:32:40 +0200

    Order page: add placeholder for movie selector add movie search

--- Scripts/order.js
+++ Scripts/order.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+  // Initialize the movie list
+  var movieList = $("#movie-select option");
+
+  // Search for a movie
+  $("#movie-search").on("input", function() {
+    var value = $(this).val().toLowerCase();
+
+    // Filter the movie list by name
+    movieList.filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+
+    // Show the "Select a movie" option if no results are found
+    if ($("#movie-select option:visible").length === 1) {
+      $("#movie-select").val("0");
+    }
+  });
+
   $("#movie-select").change(function() {
     var movieId = $("#movie-select").val();
     $.ajax({
