9860edcb66a8a925a3c815441afe70b4f0e0064b
Stefan Huang <123669812+stfnhng@users.noreply.github.com>
2023-04-13 14:34:27 +0200

    Change timeslot into buttons
    add popup after clicking the button

--- Scripts/order.js
+++ Scripts/order.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
       success: function(data) {
         var timeslots = "";
         $.each(data, function(index, value) {
-          timeslots += "<li>" + value.time + "</li>";
+          timeslots += "<button class='timeslot-button' data-time='" + value.time + "'>" + value.time + "</button>";
         });
         $("#timeslot-list").html(timeslots);
       },
@@ -34,5 +34,20 @@ $(document).ready(function() {
         alert("Error retrieving timeslots.");
       }
     });
+  });
+
+  $(document).on("click", ".timeslot-button", function() {
+    var timeslot = $(this).data("time");
+    var amount = 1; // default amount is 1
+    var popup = "<div><p>You selected the timeslot: " + timeslot + "</p><label for='amount'>Amount:</label><input type='number' id='amount' value='1' min='1' /><button id='purchase-button'>Purchase</button></div>";
+    $("body").append(popup);
+  });
+
+  $(document).on("click", "#purchase-button", function() {
+    var timeslot = $(".timeslot-button.active").data("time");
+    var amount = $("#amount").val();
+    // TODO: send the purchase request to the server and handle the response
+    alert("You purchased " + amount + " ticket(s) for the timeslot " + timeslot);
+    $("#popup").remove();
   });
 });
